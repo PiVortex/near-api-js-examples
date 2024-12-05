93c596410c9f98a43d039a60ca584b7b6607930b
Guillermo Gallardo <gagdiez.c@gmail.com>
2024-12-05 14:22:07 +0100

    fix: summarize example

--- examples/account-details.js
+++ examples/account-details.js
@@ -1,33 +1,17 @@
-import * as nearAPI from "near-api-js";
-import dotenv from "dotenv";
+import { connect } from "near-api-js";
 
-const { connect } = nearAPI;
-
-// Load environment variables
-dotenv.config({ path: ".env" });
-const accountId = process.env.ACCOUNT_ID;
-
-// Create a connection to NEAR testnet
-const connectionConfig = {
+const connection = await connect({
   networkId: "testnet",
-  keyStore: null, // No key store needed for this example
   nodeUrl: "https://rpc.testnet.near.org",
-  walletUrl: "https://testnet.mynearwallet.com/",
-  helperUrl: "https://helper.testnet.near.org",
-  explorerUrl: "https://testnet.nearblocks.io",
-};
-const nearConnection = await connect(connectionConfig);
+});
 
-// Create an account object
-const account = await nearConnection.account(accountId); // example-account.testnet
+const account = await connection.account("influencer.testnet");
 
-// Gets the account balance in yoctoNEAR for an account
-// Includes the total balance, stateStaked, staked, and available
+// Gets the total, staked and available balance in yoctoNEAR
 const accountBalance = await account.getAccountBalance();
 console.log(accountBalance);
 
-// Returns basic account information
-// and the basic block details at which it was queried
+// Account's state, including its code_hash and storage usage
 const accountState = await account.state();
 console.log(accountState);
 
